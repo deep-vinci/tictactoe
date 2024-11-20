7d319ee4ff2f2d5ed884dbf48f6746db8de4fdd9
Deepak <deepakworkpc@gmail.com>
2024-11-20 17:01:41 +0530

    feat: only disable status div

--- main.js
+++ main.js
@@ -78,10 +78,10 @@ function gameBoard () {
     }
     
     function displayWinner(winnerObj) {
-        // winnerObj.sequence.forEach((e, i) => {
-        //     gameBoardDivs[e].classList.add("game-cell-wins");
-        // })
-        // gameBoardContainer.classList.add("pointer-events-none")
+        winnerObj.sequence.forEach((e, i) => {
+            gameBoardDivs[e].classList.add("game-cell-wins");
+        })
+        gameBoardContainer.classList.add("pointer-events-none")
         // statusBox.textContent = `Player ${winnerObj.winner} won`
         // statusBox.style.display = "block";
     }
